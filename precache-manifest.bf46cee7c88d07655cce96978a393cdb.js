34bb833f31eb45ef9ce086a0382c8d1d6d8a2b64
hadar lapidot <hadarlapidot@gmail.com>
2020-01-23 21:20:04 +0200

    Updates

---
+++ precache-manifest.bf46cee7c88d07655cce96978a393cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457ac5de2c66d098f1ceebf5a48c4824",
+    "url": "/oferlapidot/index.html"
+  },
+  {
+    "revision": "b1a44244b8dcf60151de",
+    "url": "/oferlapidot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "aab8d900da45afe883f9",
+    "url": "/oferlapidot/static/css/main.7daee9e5.chunk.css"
+  },
+  {
+    "revision": "b1a44244b8dcf60151de",
+    "url": "/oferlapidot/static/js/2.87a7badc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/oferlapidot/static/js/2.87a7badc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aab8d900da45afe883f9",
+    "url": "/oferlapidot/static/js/main.58dff727.chunk.js"
+  },
+  {
+    "revision": "062722d35e868829ebff",
+    "url": "/oferlapidot/static/js/runtime-main.adb56db4.js"
+  }
+]);
